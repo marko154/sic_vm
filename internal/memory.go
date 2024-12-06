2dc2214fe05a4843e1f2ceb716120cca3ad667c9
marko154 <marko.gartnar777@gmail.com>
2024-12-06 21:56:50 +0100

    memory functions

--- internal/memory.go
+++ internal/memory.go
@@ -1,18 +1,77 @@
 package vm
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
 
 type Memory [MAX_ADDRESS]byte
 
 const MAX_ADDRESS = 1 << 20
 
-func (mem *Memory) GetByte(address int) (byte, error) {
+func (mem *Memory) ValidateAddress(address int) error {
 	if address < 0 || address >= MAX_ADDRESS {
-		return 0, fmt.Errorf("invalid address: %d", address)
+		return fmt.Errorf("invalid address: %d", address)
+	}
+	return nil
+}
+
+func (mem *Memory) GetByte(address int) (byte, error) {
+	if err := mem.ValidateAddress(address); err != nil {
+		return 0, err
 	}
 	return mem[address], nil
 }
 
 func (mem *Memory) SetByte(address int, value byte) error {
+	if err := mem.ValidateAddress(address); err != nil {
+		return err
+	}
+	mem[address] = value
+	return nil
+}
+
+func (mem *Memory) GetWord(address int) (int, error) {
+	if err := mem.ValidateAddress(address + 2); err != nil {
+		return 0, err
+	}
+	return int(mem[address])<<16 | int(mem[address+1])<<8 | int(mem[address+2]), nil
+}
+
+func (mem *Memory) SetWord(address, value int) error {
+	if err := mem.ValidateAddress(address + 2); err != nil {
+		return err
+	}
+	mem[address] = byte(value >> 16)
+	mem[address+1] = byte(value >> 8)
+	mem[address+2] = byte(value)
+	return nil
+}
+
+// GetFloat reads a 48-bit floating point number from the memory. The floating point value is in the last 4 bytes of the 6 byte section
+func (mem *Memory) GetFloat(address int) (float32, error) {
+	if err := mem.ValidateAddress(address + 5); err != nil {
+		return 0, err
+	}
+	var value float32
+	b := mem[address+2 : address+6]
+	buf := bytes.NewReader(b)
+	// not sure if the endianess is correct
+	if err := binary.Read(buf, binary.NativeEndian, &value); err != nil {
+		return 0, err
+	}
+	return value, nil
+}
+
+func (mem *Memory) SetFloat(address int, value float32) error {
+	if err := mem.ValidateAddress(address + 5); err != nil {
+		return err
+	}
+	buf := new(bytes.Buffer)
+	if err := binary.Write(buf, binary.NativeEndian, &value); err != nil {
+		return err
+	}
+	copy(mem[address+2:], buf.Bytes())
 	return nil
 }
---
+++ internal/memory_test.go
@@ -0,0 +1,67 @@
+package vm
+
+import "testing"
+
+func TestByteOperations(t *testing.T) {
+	mem := Memory{}
+	mem.SetByte(0, 42)
+	mem.SetByte(51354, 123)
+
+	value, err := mem.GetByte(0)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if value != 42 {
+		t.Errorf("Expected 42, got %d", value)
+	}
+
+	value, err = mem.GetByte(51354)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if value != 123 {
+		t.Errorf("Expected 123, got %d", value)
+	}
+
+	err = mem.SetByte(-1, 123)
+	if err == nil {
+		t.Error("Expected an error when setting an invalid address")
+	}
+	_, err = mem.GetByte(MAX_ADDRESS + 10)
+	if err == nil {
+		t.Error("Expected an error when getting an invalid address")
+	}
+}
+
+func TestWordOperations(t *testing.T) {
+	mem := Memory{}
+	mem.SetWord(0, 0x123456)
+	mem.SetWord(12345, 0xabcdef)
+	value, err := mem.GetWord(0)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if value != 0x123456 {
+		t.Errorf("Expected 0x123456, got %x", value)
+	}
+	value, err = mem.GetWord(12345)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if value != 0xabcdef {
+		t.Errorf("Expected 0xabcdef, got %v", value)
+	}
+}
+
+func TestFloatOperation(t *testing.T) {
+	mem := Memory{}
+	mem.SetFloat(0, 3.14159)
+
+	value, err := mem.GetFloat(0)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if value != 3.14159 {
+		t.Errorf("Expected 3.14159, got %v", value)
+	}
+}
